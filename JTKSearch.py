e6674d6d42495d77dc095f82f18c32189e27d942
Lastdeve <hampus.staaf02@gmail.com>
2023-04-27 19:06:18 +0200

    Fixes

--- JTKSearch.py
+++ JTKSearch.py
@@ -125,7 +125,7 @@ def shred_folder(path):
         os.remove("Websites")
     except Exception as e:
         print('\033[91m' + f'Error shredding images or nothing was downloaded!\033[0m\n')
-        print('\033[93m' + f'Or you are running Linux, where the shredding is not supported yet. You need to shred the /websites folder yourself' + '\033[0m\n')
+        print('\033[93m' + f'Or you are running Linux, where the shredding is not supported yet. You need to shred the /websites folder yourself!' + '\033[0m\n')
 
 shred_folder('./Websites\n')
 
